b04b0c97ae488c901c2b37972e85b24ac753b6d8
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-05-16 14:19:38 +1000

    trigger build

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world 4\n');
+  res.send('Hello world, this is Kerry and Gus!\n');
 });
 
 app.listen(PORT);
