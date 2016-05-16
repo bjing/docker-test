d44a86156358b766c2455d5ec493fc6112387771
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-05-16 16:40:22 +1000

    whitespace problems?

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -7,17 +7,17 @@ REGION=us-west-1
 CLUSTER=app
 SERVICE=app
 
-docker run --rm anigeo/awscli 		\
-   ecs register-task-definition		\
-   --family $FAMILY					\
-   --region $REGION 				\
+docker run --rm anigeo/awscli \
+   ecs register-task-definition \
+   --family $FAMILY \
+   --region $REGION \
    --container-definitions "[{\"name\":\"hello-world\",\"image\":\"${IMAGE}:${TAG}\",\"cpu\":0,\"memory\":200,\"essential\":true,\"portMappings\": [{\"containerPort\": 8080,\"hostPort\": 80 }]}]" | grep "revision" | cut -d ':' -f 2 | cut -c2- > revision.txt
-      
-docker run --rm anigeo/awscli 		\
-   ecs update-service 				\
-   --cluster $CLUSTER 				\
-   --service $APP 					\
-   --region $REGION 				\
+
+docker run --rm anigeo/awscli  \
+   ecs update-service \
+   --cluster $CLUSTER 
+   --service $APP \
+   --region $REGION \
    --task-definition "${FAMILY}:$(cat revision.txt)"
 
 # Original Jenkins Job
