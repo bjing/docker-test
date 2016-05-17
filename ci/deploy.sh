56c64817383fb1f3c26b9f528e775e15495f42f7
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-05-17 15:04:12 +1000

    Deploy improvements

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -17,8 +17,8 @@ IMAGE_URL=$IMAGE
 IMAGE_TAG=$TAG
 MEMORY=200
 
-PATH=$(pwd)
-TEMPLATE=$(cat ./ci/task-definition.json.template)
+TEMPLATE=$(cat ci/task-definition.json.template)
+echo $TEMPLATE
 
 GENERATED=${TEMPLATE//HOST_PORT/$HOST_PORT}
 GENERATED=${GENERATED//CONTAINER_PORT/$CONTAINER_PORT}
@@ -27,13 +27,17 @@ GENERATED=${GENERATED//IMAGE_URL/$IMAGE_URL}
 GENERATED=${GENERATED//IMAGE_TAG/$IMAGE_TAG}
 GENERATED=${GENERATED//MEMORY/$MEMORY}
 
+echo "$GENERATED" > /tmp/task-definition.json
+
 echo "Creating task-definition for tag: ${TAG}"
 docker run --rm anigeo/awscli      \
    ecs register-task-definition    \
    --family $FAMILY                \
    --region $REGION                \
-   --container-definitions $GENERATED | grep "revision" | cut -d ':' -f 2 | cut -c2- > revision.txt
+   --cli-input-json file:///tmp/task-definition.json | grep "revision" | cut -d ':' -f 2 | cut -c2- > revision.txt
    
 echo "Updating service with task-definition: $(cat revision.txt)"
 docker run --rm anigeo/awscli \
    ecs update-service --cluster $CLUSTER --service $SERVICE --region $REGION --task-definition "${FAMILY}:$(cat revision.txt)"
+
+rm /tmp/task-definition.json
