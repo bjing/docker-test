f10a593691e1c7facf1bbe789cd9b52fbbbf5373
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-05-17 14:18:27 +1000

    Generate container definitions from template file

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -9,27 +9,29 @@ REGION=us-west-1
 CLUSTER=app
 SERVICE=app
 
-# docker run --rm anigeo/awscli \
-#    ecs register-task-definition \
-#    --family $FAMILY \
-#    --region $REGION \
-#    --container-definitions "[{\"name\":\"hello-world\",\"image\":\"${IMAGE}:${TAG}\",\"cpu\":0,\"memory\":200,\"essential\":true,\"portMappings\": [{\"containerPort\": 8080,\"hostPort\": 80 }]}]" | grep "revision" | cut -d ':' -f 2 | cut -c2- > revision.txt
+echo "Generating task-definition.json from template file"
+HOST_PORT=80
+CONTAINER_PORT=8080
+CONTAINER_NAME="hello-world"
+IMAGE_URL=$IMAGE
+IMAGE_TAG=$TAG
+MEMORY=200
 
-# docker run --rm anigeo/awscli  \
-#    ecs update-service \
-#    --cluster $CLUSTER 
-#    --service $APP \
-#    --region $REGION \
-#    --task-definition "${FAMILY}:$(cat revision.txt)"
+TEMPLATE=$(cat task-definition.json.template)
 
-# Original Jenkins Job
+GENERATED=${TEMPLATE//HOST_PORT/$HOST_PORT}
+GENERATED=${GENERATED//CONTAINER_PORT/$CONTAINER_PORT}
+GENERATED=${GENERATED//CONTAINER_NAME/$CONTAINER_NAME}
+GENERATED=${GENERATED//IMAGE_URL/$IMAGE_URL}
+GENERATED=${GENERATED//IMAGE_TAG/$IMAGE_TAG}
+GENERATED=${GENERATED//MEMORY/$MEMORY}
 
 echo "Creating task-definition for tag: ${TAG}"
 docker run --rm anigeo/awscli      \
    ecs register-task-definition    \
    --family $FAMILY                \
    --region $REGION                \
-   --container-definitions "[{\"name\":\"hello-world\",\"image\":\"${IMAGE}:${TAG}\",\"cpu\":0,\"memory\":200,\"essential\":true,\"portMappings\": [{\"containerPort\": 8080,\"hostPort\": 80 }]}]" | grep "revision" | cut -d ':' -f 2 | cut -c2- > revision.txt
+   --container-definitions $GENERATED | grep "revision" | cut -d ':' -f 2 | cut -c2- > revision.txt
    
 echo "Updating service with task-definition: $(cat revision.txt)"
 docker run --rm anigeo/awscli \
