30443a62e8a4c90276d12b6985a40fd5100943ca
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-05-17 10:08:21 +1000

    More verbose smoke test

--- ci/smoke.sh
+++ ci/smoke.sh
@@ -7,8 +7,11 @@ SERVICE=app
 REGION=us-west-1
 URL=http://app-ecs-elb-659670922.us-west-1.elb.amazonaws.com/
 
+echo -n "Waiting for service to stabilize..."
 docker run anigeo/awscli ecs wait services-stable --cluster $CLUSTER --services $SERVICE --region $REGION
+echo "   stable"
 
+echo "Trying to reach ${URL} 60 times with a 10 second wait between each attempt"
 COUNTER=60
 while [ $COUNTER -gt 0 ]
 do
@@ -23,5 +26,5 @@ do
 	sleep 10
 done
 
-echo "Failed to reach app after 10minutes."
+echo "Failed to reach ${URL} after 10 minutes."
 exit 1
