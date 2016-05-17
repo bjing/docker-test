4e2df5c37e62a2b313e5ea98f2c01c94eefb0652
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-05-17 10:46:06 +1000

    push

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var PORT = 8080;
 // App
 var app = express();
 app.get('/', function (req, res) {
-  res.send('Hello world, this is Kerry and Gus!\n');
+  res.send('Hello world, this is all of us!\n');
 });
 
 app.listen(PORT);
