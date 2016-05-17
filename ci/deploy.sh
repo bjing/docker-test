f9914586e1d59ce50d0287ebd94d15a1567fe18c
Gustavo Hoirisch <guga@maurorisch.com.br>
2016-05-17 15:13:25 +1000

    debug

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -26,18 +26,18 @@ GENERATED=${GENERATED//IMAGE_URL/$IMAGE_URL}
 GENERATED=${GENERATED//IMAGE_TAG/$IMAGE_TAG}
 GENERATED=${GENERATED//MEMORY/$MEMORY}
 
-echo "$GENERATED" > /tmp/task-definition.json
-cat /tmp/task-definition.json
+echo "$GENERATED" > ci/task-definition.json
+cat ci/task-definition.json
 
 echo "Creating task-definition for tag: ${TAG}"
 docker run --rm anigeo/awscli      \
    ecs register-task-definition    \
    --family $FAMILY                \
    --region $REGION                \
-   --cli-input-json file:///tmp/task-definition.json | grep "revision" | cut -d ':' -f 2 | cut -c2- > revision.txt
+   --cli-input-json file://ci/task-definition.json | grep "revision" | cut -d ':' -f 2 | cut -c2- > revision.txt
    
 echo "Updating service with task-definition: $(cat revision.txt)"
 docker run --rm anigeo/awscli \
    ecs update-service --cluster $CLUSTER --service $SERVICE --region $REGION --task-definition "${FAMILY}:$(cat revision.txt)"
 
-rm /tmp/task-definition.json
+rm ci/task-definition.json
